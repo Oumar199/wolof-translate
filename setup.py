d9fecf5f9eae7d5e58b664eec72075c63ad5fb60
= <=>
2024-03-04 13:17:43 +0000

    adding setup

--- setup.py
+++ setup.py
@@ -2,7 +2,4 @@ from setuptools import setup
 
 setup(name="wolof_translate", version="0.0.1", author="Oumar Kane", author_email="oumar.kane@univ-thies.sn", 
       description="Contain function and classes to process corpora for making translation between wolof text and other languages.",
-      requires=['spacy', 'nltk', 'gensim', 'furo', 'myst-parser', 'Sphinx==6.2.1', 'nbsphinx', 
-                'streamlit', 'tokenizers', 'evaluate', 'torch', 'transformers', 'sphinx-autobuild',
-                'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'plotly', 'sacrebleu', 'tensorboard',
-                'nlpaug', 'wandb', 'pytorch_lightning', 'selenium', 'sentencepiece'])
+      install_requires=['spacy', 'nltk', 'gensim', 'furo', 'nbsphinx', 'streamlit', 'tokenizers', 'evaluate', 'torch', 'transformers', 'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'plotly', 'sacrebleu', 'tensorboard', 'nlpaug', 'wandb', 'pytorch_lightning', 'selenium', 'sentencepiece'])
