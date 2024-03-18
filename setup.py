18f90b522825e24b07f21022339a426018ae0e6b
Oumar Kane <oumar.kane@univ-thies.sn>
2024-03-18 19:24:16 +0000

    adding setup

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 
 setup(name="wolof_translate", version="0.0.1", author="Oumar Kane", author_email="oumar.kane@univ-thies.sn", 
       description="Contain function and classes to process corpora for making translation between wolof text and other languages.",
-      install_requires=['spacy', 'nltk', 'gensim', 'furo', 'nbsphinx', 'streamlit', 'tokenizers', 'evaluate', 'torch', 'transformers', 'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'plotly', 'sacrebleu', 'tensorboard', 'nlpaug', 'wandb', 'pytorch_lightning', 'selenium', 'sentencepiece'])
+      install_requires=['spacy', 'nltk', 'gensim', 'furo', 'nbsphinx', 'streamlit', 'tokenizers', 'evaluate', 'transformers', 'pandas', 'numpy', 'scikit-learn', 'matplotlib', 'plotly', 'sacrebleu', 'nlpaug', 'wandb', 'pytorch_lightning', 'selenium', 'sentencepiece'])
